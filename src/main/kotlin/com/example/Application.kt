776b5c117f0de2f94b0661f79f052dbb7dba6c2b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-01-30 21:00:00 +0900

    feat: Apply ktlint and formatting

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,8 +1,8 @@
 package com.example
 
-import com.example.plugins.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import com.example.plugins.configureRouting
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") { configureRouting() }.start(wait = true)
