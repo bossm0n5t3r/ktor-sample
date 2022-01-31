8f1975e81929bbe6efe64f9d26e48a31d567fa9f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-02-01 00:07:39 +0900

    feat: Add CustomerRoutes

---
+++ src/main/kotlin/com/example/httpapi/routes/CustomerRoutes.kt
@@ -0,0 +1,30 @@
+package com.example.httpapi.routes
+
+import com.example.httpapi.models.customerStorage
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.Route
+import io.ktor.routing.delete
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.route
+
+fun Route.customerRouting() {
+    route("/customer") {
+        get {
+            if (customerStorage.isNotEmpty()) {
+                call.respond(customerStorage)
+            } else {
+                call.respondText("No customers found", status = HttpStatusCode.NotFound)
+            }
+        }
+        get("{id}") {
+        }
+        post {
+        }
+        delete("{id}") {
+        }
+    }
+}
