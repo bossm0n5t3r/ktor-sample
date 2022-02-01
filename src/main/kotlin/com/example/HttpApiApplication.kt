7ba208bdeee508acbcec87f1423d71bf48e20485
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-02-01 16:47:40 +0900

    feat: Edit HttpApiApplication.kt
    - Add registerOrderRoutes
    - install CORS for anyHost()

--- src/main/kotlin/com/example/HttpApiApplication.kt
+++ src/main/kotlin/com/example/HttpApiApplication.kt
@@ -1,17 +1,23 @@
 package com.example
 
 import com.example.httpapi.routes.registerCustomerRoutes
+import com.example.httpapi.routes.registerOrderRoutes
 import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.features.CORS
 import io.ktor.features.ContentNegotiation
 import io.ktor.serialization.json
 import io.ktor.server.netty.EngineMain
 
 fun main(args: Array<String>) = EngineMain.main(args)
 
-fun Application.module() {
+fun Application.module(testing: Boolean = false) {
     install(ContentNegotiation) {
         json()
     }
+    install(CORS) {
+        anyHost()
+    }
     registerCustomerRoutes()
+    registerOrderRoutes()
 }
