f3d013784eccfee4c46adfb5bba08d65d541ba8d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-02-10 00:23:30 +0900

    feat: Update WebsiteApplication
    - Add post /submit

--- src/main/kotlin/com/example/WebsiteApplication.kt
+++ src/main/kotlin/com/example/WebsiteApplication.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import com.example.website.model.BlogEntry
 import com.example.website.model.blogEntries
 import freemarker.cache.ClassTemplateLoader
 import freemarker.core.HTMLOutputFormat
@@ -8,12 +9,21 @@ import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.freemarker.FreeMarker
 import io.ktor.freemarker.FreeMarkerContent
+import io.ktor.html.respondHtml
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
+import io.ktor.request.receiveParameters
 import io.ktor.response.respond
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.netty.EngineMain
+import kotlinx.html.a
+import kotlinx.html.b
+import kotlinx.html.body
+import kotlinx.html.h1
+import kotlinx.html.p
 
 fun main(args: Array<String>) = EngineMain.main(args)
 
@@ -28,6 +38,32 @@ fun Application.module() {
         }
         get("/") {
             call.respond(FreeMarkerContent("index.ftl", mapOf("entries" to blogEntries), ""))
+        }
+        post("/submit") {
+            val params = call.receiveParameters()
+            val headline = params["headline"] ?: return@post call.respond(HttpStatusCode.BadRequest)
+            val body = params["body"] ?: return@post call.respond(HttpStatusCode.BadRequest)
+            val newEntry = BlogEntry(headline, body)
+            blogEntries.add(0, newEntry)
+            call.respondHtml {
+                body {
+                    h1 {
+                        +"Thanks for submitting your entry!"
+                    }
+                    p {
+                        +"We've submitted your new entry titled "
+                        b {
+                            +newEntry.headline
+                        }
+                    }
+                    p {
+                        +"You have submitted a total of ${blogEntries.count()} articles!"
+                    }
+                    a("/") {
+                        +"Go back"
+                    }
+                }
+            }
         }
     }
 }
