35d467b0ab2b7bdce6139578ad0bd97c3f6e723f
bossm0n5t3r <bossm0n5t3r@gmail.com>
2022-02-10 00:07:33 +0900

    feat: Update WebsiteApplication
    - Add get /
      - call respond FreeMarkerContent

--- src/main/kotlin/com/example/WebsiteApplication.kt
+++ src/main/kotlin/com/example/WebsiteApplication.kt
@@ -1,17 +1,33 @@
 package com.example
 
+import com.example.website.model.blogEntries
+import freemarker.cache.ClassTemplateLoader
+import freemarker.core.HTMLOutputFormat
 import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.freemarker.FreeMarker
+import io.ktor.freemarker.FreeMarkerContent
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
+import io.ktor.response.respond
+import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.netty.EngineMain
 
 fun main(args: Array<String>) = EngineMain.main(args)
 
 fun Application.module() {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        outputFormat = HTMLOutputFormat.INSTANCE
+    }
     routing {
         static("/static") {
             resources("files")
+        }
+        get("/") {
+            call.respond(FreeMarkerContent("index.ftl", mapOf("entries" to blogEntries), ""))
         }
     }
 }
